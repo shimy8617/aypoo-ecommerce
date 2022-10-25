9f0a37216308253d0d61c83a14b0bfdee4c71bb1
shimy8617 <cristinashim@yahoo.com.ar>
2022-10-24 23:01:08 -0300

    me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
