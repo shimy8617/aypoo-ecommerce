c4278a9f80096afdf6dba4074ea04f29c2a564c1
shimy8617 <cristinashim@yahoo.com.ar>
2022-10-25 09:56:32 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navigation from './routes/navigation/navigation'
 import Home from "./routes/home/home";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element={<Navigation />}> 
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
